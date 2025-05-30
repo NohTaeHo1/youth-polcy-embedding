337ed1b577fef74494b63b01c6ded8a35f4a4ea3
RM-jinsan <yug6789@naver.com>
2025-05-30 14:43:56 +0900

    [service] kjs 청년정책 LLM 서비스를 위한 로컬 환경 구성과 환경 정리 중 오류 작업 수정 진행중.

--- BE/app/config.py
+++ BE/app/config.py
@@ -1,10 +1,8 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 MONGO_URI = os.getenv("MONGODB_URL")
 ONTO_API_KEY = os.getenv("ONTO_API_KEY")
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
 
 if not MONGO_URI:
     raise ValueError("MONGODB_URL is not set in .env")
